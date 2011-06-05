7a1c81e67191aad638ac6d5cb8b3e3e762ac35ab
Michele Mattioni <mattioni@ebi.ac.uk>
2011-06-05 22:12:39 +0100

    Modified the model to use the gis infrastructure.

--- apps/map/models.py
+++ apps/map/models.py
@@ -1,10 +1,18 @@
-from django.db import models
+from django.contrib.gis.db import models
 from django.contrib.auth.models import User
 
 
 class Location(models.Model):
+    """Class for the location which couple with the User.
+    If you curious about the SRID read here: https://secure.wikimedia.org/wikipedia/en/wiki/SRID
+    Of you want to know why 4326 and not 42 read here: 
+        https://secure.wikimedia.org/wikipedia/en/wiki/WGS84
+    """
 
-    user = models.ForeignKey(User, related_name='location_creator_set')
-    name = models.CharField(max_length=100)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    user = models.ForeignKey(User, related_name='location_creator_set')    
+    name = models.CharField(max_length=32)
+    geometry = models.PointField(srid=4326)
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return '%s %s %s' % (self.name, self.geometry.x, self.geometry.y)
