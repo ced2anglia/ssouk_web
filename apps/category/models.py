075507b54c4c378d629bb3ac40a0a5c84a04a38a
Michele Mattioni <mattioni@ebi.ac.uk>
2011-06-12 13:20:46 +0100

    Creating the model for the category app.

--- apps/category/models.py
+++ apps/category/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+CATEGORY_TYPE = (
+                 ('V', 'Vegetables',
+                  'F', 'Fruit'
+                  'M', 'Meat'
+                  'Fi' 'Fish',
+                  'D', 'Dairy',
+                  'B', 'Beverages',
+                  'G', 'Garden',
+                  'O', 'Other' 
+                  )
+                 )
 # Create your models here.
+class Category(models.Model):
+    name =  models.CharField(max_length=10)
+    image = Model.ImageField(blank=True)
