4cfa77faa5c799a3a9bd1651a5af4b1dca6fb7b9
Michele Mattioni <mattioni@ebi.ac.uk>
2011-06-19 11:05:09 +0100

    Added the geodjango extension to the map.

--- ssouk/apps/map/admin.py
+++ ssouk/apps/map/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 from apps.map.models import Location
 
-admin.site.register(Location)
+admin.site.register(Location, admin.GeoModelAdmin)
