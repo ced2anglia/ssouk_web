444bd028f3858c93df7c22a86d7f31e6f76730a0
Michele Mattioni <mattioni@ebi.ac.uk>
2011-06-24 10:54:28 +0100

    Styled the form with a new id_price_info to insert Js computed text. The JS is not loading properly on the page somehow..

--- ssouk/apps/inventory/static/js/inventory_math.js
+++ ssouk/apps/inventory/static/js/inventory_math.js
@@ -1,15 +1,15 @@
 $(document).ready(function (){
     var quantity,
         price;
-
+	alert('hey!');
     $('#id_quantity').keyup(function (){
         quantity = parseInt($('#id_quantity').val());
     });
 
     $('#id_price').keyup(function (){
-        price = parseInt($('#id_price').val());
+        price = parseFloat($('#id_price').val());
         if (quantity && price) {
-        	$('#id_price').after(price/quantity + ' per item' + ' yeah!');
+            $('#id_price_info').html(price/quantity + ' per item');
         }
     });
 });
