4ae1b9cf98e3ec7024cd5b04605c722e86f88b6a
Michele Mattioni <mattioni@ebi.ac.uk>
2011-10-19 15:34:01 +0100

    Inserted the Layout object. Now we can add the link for a new location.

--- ssouk/apps/inventory/forms.py
+++ ssouk/apps/inventory/forms.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.forms import ModelForm
 from apps.inventory.models import Item
 from uni_form.helpers import FormHelper
-from uni_form.layout import Submit, Reset
+from uni_form.layout import Submit, Reset, Fieldset, Layout
 
 
 class ItemForm(ModelForm):
-    error_css_class = 'error'
-    required_css_class = 'required'
+
     class Meta:
         model = Item
         exclude = ('creation_date',
@@ -24,5 +23,21 @@ class ItemForm(ModelForm):
         self.helper.add_input(Reset('reset','Reset'))
         self.helper.add_input(Submit('submit','Submit'))
         self.user = user
+        self.helper.layout = Layout(
+            Fieldset(
+                'legend name',
+                'name',
+                'category', 
+                'description',
+                'location',
+                'expire_date',
+                'quantity',
+                'quantity_type',
+                'sell_individually',
+                'swappable',
+                'price',
+                'notes'
+            )
+        )
         super(ItemForm, self).__init__(*args, **kwargs)
         
