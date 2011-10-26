74710d40ba0e60bd6ed1a91a89b11ba11b0f0047
Michele Mattioni <mattioni@ebi.ac.uk>
2011-10-26 01:58:29 +0100

    Inserted the slash at the end of the URL, as used in best practised.

--- ssouk/apps/ajax_example/urls.py
+++ ssouk/apps/ajax_example/urls.py
@@ -2,6 +2,6 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('ajax_example.views',
-    url(r'^/xhr_test$','xhr_test'),
+    url(r'^xhr_test$','xhr_test'),
     url('^$', 'index'),
 )
