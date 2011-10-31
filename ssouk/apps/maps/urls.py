79886079f86968af0f346d7b5db7487a9b19004a
Michele Mattioni <mattioni@ebi.ac.uk>
2011-10-31 22:26:54 +0000

    Using the username directly from the request, not from the url.

--- ssouk/apps/maps/urls.py
+++ ssouk/apps/maps/urls.py
@@ -2,5 +2,5 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('maps.views',
-                       url(r'^(?P<username>\w+)/locations', 'list', name='locations_list'),
+                       url(r'^$', 'list', name='locations_list'),
 )
--- ssouk/apps/maps/views.py
+++ ssouk/apps/maps/views.py
@@ -2,29 +2,27 @@ from django.template import RequestContext
 from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
+from django.core.urlresolvers import reverse
+
 from utils import calculate_center
+
 
 import logging
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 
-def list(request, username, template='maps/locations.html'):
+def list(request, template='maps/locations.html'):
     data = {}
     if request.user.is_authenticated():
-        if username == request.user.username:
-                
-                locations_obj = request.user.location_set.all()
-                data['locations'] = locations_obj
-                data['username'] = username
-                data['center'] = calculate_center(locations_obj)
-                return render_to_response(template,
-                              data,
-                              context_instance=RequestContext(request)
-                              )
-        else:
-            pass
-            # User logged in but not owner of the url.
-            # We should redirect her to her own locations.
-            # TODO
-            # HttpResponseRedirect()     
+        
+        locations_obj = request.user.location_set.all()
+        data['locations'] = locations_obj
+        data['username'] = request.user.username
+        data['center'] = calculate_center(locations_obj)
+        return render_to_response(template,
+                      data,
+                      context_instance=RequestContext(request)
+                      )
+    else:
+        return(HttpResponseRedirect(reverse('general_customer')))
