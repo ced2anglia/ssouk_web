2696422d9be1ef1cd788330c912ac8c7f4363825
Michele Mattioni <mattioni@ebi.ac.uk>
2011-10-31 22:37:58 +0000

    Using the login_required decorator to force the login to edit the map.

--- ssouk/apps/maps/views.py
+++ ssouk/apps/maps/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 
 from utils import calculate_center
 
@@ -11,18 +12,22 @@ import logging
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
+@login_required
 def list(request, template='maps/locations.html'):
+    
     data = {}
-    if request.user.is_authenticated():
-        
-        locations_obj = request.user.location_set.all()
-        data['locations'] = locations_obj
-        data['username'] = request.user.username
-        data['center'] = calculate_center(locations_obj)
-        return render_to_response(template,
-                      data,
-                      context_instance=RequestContext(request)
-                      )
-    else:
-        return(HttpResponseRedirect(reverse('general_customer')))
+    locations_obj = request.user.location_set.all()
+    data['locations'] = locations_obj
+    data['username'] = request.user.username
+    data['center'] = calculate_center(locations_obj)
+    
+    return render_to_response(template,
+                  data,
+                  context_instance=RequestContext(request)
+                  )
+
+@login_required    
+def add(request):
+    data = {}
+    
+    
