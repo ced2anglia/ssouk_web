30b1e4c2b534dafecd1c9d1a1ec33a3c6e0230c5
Michele Mattioni <mattioni@ebi.ac.uk>
2012-04-25 17:44:32 +0100

    Working on the RESTful API to integrate with backbone.

--- ssouk/api.py
+++ ssouk/api.py
@@ -1,6 +1,10 @@
 from tastypie.resources import ModelResource
 from inventory.models import Item
 from django.contrib.auth.models import User
+from tastypie import fields
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
+from tastypie.authentication import BasicAuthentication
+from tastypie.authorization import DjangoAuthorization
 
 
 class UserResource(ModelResource):
@@ -8,10 +12,24 @@ class UserResource(ModelResource):
         queryset = User.objects.all()
         resource = "user"
         excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
+        filtering = {
+            'username': ALL,
+        }
+        #authentication = BasicAuthentication()
+        #authorization = DjangoAuthorization()
         
 
 class ItemResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'user')
+    
     class Meta:
         queryset = Item.objects.all()
         resource = "item"
+        filtering = {
+            'user': ALL_WITH_RELATIONS,
+            'name': ALL,
+            'expire_date': ['exact', 'lt', 'lte', 'gte', 'gt'],
+        }
+        #authentication = BasicAuthentication()
+        #authorization = DjangoAuthorization()
         
