e5f8e615e5acbcf5a3e79e16555e7d6a52e242ed
Michele Mattioni <mattioni@ebi.ac.uk>
2012-04-27 13:41:56 +0100

    Switching to leaflet for the map.

---
+++ ssouk/apps/maps/static/js/map-leaflet.js
@@ -0,0 +1,57 @@
+
+SSOUK.namespace('SSOUK.map_handler');
+
+SSOUK.map_handler = function() {
+    
+    init = function(center_x, center_y) {
+        var y =  52.20005469158063,
+            x = 0.12249999999994543,
+            zoom = 13;
+            
+        var map = new L.Map('map_canvas');
+        var mapquestUrl = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
+        subDomains = ['otile1','otile2','otile3','otile4'],
+        mapquestAttrib = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" \ target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.';
+                
+        var mapquest = new L.TileLayer(mapquestUrl, {maxZoom: 18, attribution: mapquestAttrib, subdomains: subDomains});
+        
+        map.addLayer(mapquest).setView(new L.LatLng(y, x), zoom);
+        
+        map.on('dragend', SSOUK.map_handler.updateDisplay);
+        map.on('zoomend', SSOUK.map_handler.updateDisplay);
+        SSOUK.map_handler.map = map;
+   }
+   
+    updateMarker = function(lat, lng) {
+        
+        var marker = new L.Marker(new L.LatLng(lat, lng));
+        map = SSOUK.map_handler.map;
+        map.addLayer(marker);
+    }
+
+   updateDisplay =  function(e) {
+        
+        bounds = SSOUK.map_handler.map.getBounds();
+        var sw = bounds.getSouthWest();
+        var ne = bounds.getNorthEast();
+        
+        // get the items from django
+        // and update the items 
+        $.get("/inventory/get_items_within_map", {
+            'sw_y' : sw.lat,
+            'sw_x' : sw.lng,
+            'ne_y' : ne.lat,
+            'ne_x' : ne.lng
+        }, function (html_response) {
+            $('#inventory-list').replaceWith(html_response);
+        });
+   }
+   
+   
+   return {
+        init : init,
+        updateDisplay : updateDisplay,
+        updateMarker : updateMarker, 
+   }
+}();
+ 
