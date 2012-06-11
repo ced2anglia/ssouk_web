5669b4bdc0c651fdfa00ffaaa1db2753b1475fd1
cedric <cedric.deur@gmail.com>
2012-06-11 22:26:39 +0200

    Update master

--- ssouk/apps/profiles/models.py
+++ ssouk/apps/profiles/models.py
@@ -7,6 +7,13 @@ from idios.models import ProfileBase
 class Profile(ProfileBase):
     name = models.CharField(_("name"), max_length=50, null=True, blank=True)
     
+    university = (
+        ('Cambridge', 'Cambridge'),
+        ('Bedforshire', 'Bedforshire'),
+        ('Hertfordshire', 'Hertfordshire'),
+    )
+    university = models.CharField(_("university"), choices= university)
+    
     studies= models.TextField(_("studies"), null=True, blank=True)
     about = models.TextField(_("about"), null=True, blank=True)
     location = models.CharField(_("location"), max_length=40, null=True, blank=True)
